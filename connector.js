1c7f619165610b5285a2e0cfef75d4779feb4fc9
Akashchandre <chandreakash@gmail.com>
2024-12-30 18:30:31 +0530

    dep

--- connector.js
+++ connector.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'zomato-orders.cjmsgyqw88a2.eu-north-1.rds.amazonaws.com',
-  user: 'admin', 
-  password: 'Chandre08', 
-  database: 'zomato_orders' 
+  host: process.env.DB_HOST || 'zomato-orders.cjmsgyqw88a2.eu-north-1.rds.amazonaws.com',
+  user: process.env.DB_USER || 'admin',
+  password: process.env.DB_PASSWORD || 'Chandre08',
+  database: process.env.DB_NAME || 'zomato_orders'
 });
 
 connection.connect((err) => {
